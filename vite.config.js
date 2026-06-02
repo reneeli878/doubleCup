fee0b806b046bba196be98d19d1cd4a9544eccbf
reneeli878.github.io <reneeli878@gmail.com>
2026-06-03 02:52:14 +0800

    setup gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 export default defineConfig({
   plugins: [vue()],
-  base: '/OSU.github.io/',
+  base: '/doubleCup/'  // ← 關鍵！
 })
